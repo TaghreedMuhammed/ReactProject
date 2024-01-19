99f0fd70963f1a4d9e887b0f5c7a4dd8aef648df
EmanAmr1 <emanamr543@gmail.com>
2024-01-20 00:19:52 +0200

    add StarRating.js file to handle rating of product

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Card, CardGroup } from 'react-bootstrap';
 import { axiosInstance } from '../apis/config';
 import './ProductDetailsCss.css'
-
+import StarRating from './StarRating';
 const ProductDetails = () => {
 
     const [product, setProduct] = useState(null);
@@ -35,12 +35,12 @@ const ProductDetails = () => {
 
     return (
         <>
-            <div className='contanier'>
+            <div className='contanier all'>
                 <div className='row mt-5'>
-                    <div className='d-flex flex-lg-row flex-md-column flex-sm-column  col-12'>
+                    <div className='d-flex flex-lg-row flex-md-column  flex-sm-column flex-row  '>
 
-                        <div className='  col-lg-6 col-md-12 col-sm-12 '>
-                            <div className='proThumbnail'> <img className='proImg' src={product.thumbnail} alt='' /></div>
+                        <div className=' col-lg-6  col-md-12 col-sm-12  '>
+                            <div className='proThumbnail '> <img className='proImg' src={product.thumbnail} alt='' /></div>
 
                             <CardGroup className='mt-2'>
                                 {product.images.map((imageUrl, index) => (
@@ -51,17 +51,19 @@ const ProductDetails = () => {
                             </CardGroup>
 
 
+                           
+
                         </div >
-                        <div className='offset-1 title'>
+                        <div className='offset-lg-1  offset-md-0  offset-sm-0   offset-1 col-lg-4 col-md-12 col-sm-12 title'>
 
 
                             <div className=' d-flex flex-column'>
                                 <h1>{product.title}</h1>
                                 <p>{product.description}</p>
-                                
-                                <hr className='mt-3 me-3'></hr>
+                                <p> <StarRating rating={product.rating} /></p>
+                                <hr className='mt-lg-3 '></hr>
                                 <h2> {formatPrice(product.price)} or ${calculateMonthlyPayment(product.price, product.discountPercentage, 6)}/month</h2>
-                                <hr className='mt-3 me-3'></hr>
+                                <hr className='mt-lg-3 '></hr>
 
                                 <button className='stock'> {product.stock !== 0}In stock</button>
                                 <p>More Information</p>
@@ -69,7 +71,7 @@ const ProductDetails = () => {
                                     <button className='category'> {product.category}</button>
                                     <button className='category'> {product.brand}</button></div>
                             </div>
-                            <hr className='mt-3 me-3'></hr>
+                            <hr className='mt-3 '></hr>
 
                             <div className='d-flex flex-row'> {product.stock !== 0 ?
                                 (<> <button className='category'> - 1 +</button>
@@ -80,7 +82,7 @@ const ProductDetails = () => {
 
 
                             <div><button className='mt-1 add'>Add to cart</button></div>
-                            <hr className='mt-3 me-3'></hr>
+                            <hr className='mt-3 '></hr>
 
                         </div>
 
