bef3e84f2c06453090cab896596bf3d848dcc5e6
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-01-20 03:06:59 +0200

    paginate

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,16 +1,18 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import './products.css';
 import { axiosInstance } from '../apis/config';
 import { useNavigate } from 'react-router-dom';
-
+import Pagination from "react-js-pagination";
+import './ProductsList.css';
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
+  const itemsPerPage = 6; // Number of items to display per page
 
   useEffect(() => {
     axiosInstance
-    .get('/products')
+      .get('/products')
       .then((res) => {
         setProducts(res.data.products);
       })
@@ -19,11 +21,16 @@ function ProductsList() {
       });
   }, []);
 
+  const [currentPage, setCurrentPage] = useState(1);
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const dataPerPage = products.slice(startIndex, endIndex);
 
-
-  console.log(products);
   return (
     <div className="container">
       <p style={{ marginTop: "10px" }}><b>Welcome To Our Store, Start browsing...</b></p>
@@ -43,30 +50,41 @@ function ProductsList() {
         </div>
 
         <div className="row row-cols-1 row-cols-md-3 g-4 mt-3">
-          {products.map(product => (
+          {dataPerPage.map(product => (
             <div className="col" key={product.id}>
               <div className="card">
                 <img src={`https://cdn.dummyjson.com/product-images/${product.id}/thumbnail.jpg`} className="card-img-top" alt="{product.title}" style={{height:"200px",width:"355px"}} />
                 <div className="card-body">
-                <p className="card-text">{
-                    product.stock > 0 ? 
-                    <span style={{color: 'green'}}>In Stock</span> : 
-                    <span style={{color: 'red'}}>Out of Stock</span>
-                  }</p>
+                  <p className="card-text">
+                    {product.stock > 0 ? 
+                      <span style={{color: 'green'}}>In Stock</span> : 
+                      <span style={{color: 'red'}}>Out of Stock</span>
+                    }
+                  </p>
                   <p className="card-title"><b>{product.title}</b></p>
-                  {/* <p className="card-text">{product.category}</p> */}
-                  <p className="card-text">Price:{product.price}$</p>
-                  <p className="card-text"><b>Rating:{product.rating}</b></p>
+                  <p className="card-text">Price: {product.price}$</p>
+                  <p className="card-text"><b>Rating: {product.rating}</b></p>
 
-                  <button   onClick={() => navigate(`/ProductDetails/${product.id}`)}  >ProductDetails</button>
-                    
+                  <button onClick={() => navigate(`/ProductDetails/${product.id}`)}>ProductDetails</button>
                   <button className="cart">Add To Cart</button>
-                  
                 </div>
               </div>
             </div>
           ))}
         </div>
+
+        <div className="pagination-container">
+      <Pagination 
+        activePage={currentPage} 
+        itemsCountPerPage={itemsPerPage} 
+        totalItemsCount={products.length} 
+        pageRangeDisplayed={5} 
+        onChange={handlePageChange} 
+        itemClass="pagination-item"
+        linkClass="pagination-link"
+        activeLinkClass="pagination-link active" // Add this line to style the active page
+      />
+    </div>
       </div>
     </div>
   );
